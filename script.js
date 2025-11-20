8a4cc6851c0d9f03bd162113ca42ffaf6a67b748
mdsamir7050 <xposedfuture@gmail.com>
2025-11-20 19:41:23 +0530

    first upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+}); 
